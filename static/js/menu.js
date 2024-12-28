3af63fc260a7516092f11e5ba7a8cab9227ec14d
compuser21 <137155283+compuser21@users.noreply.github.com>
2024-12-28 13:58:43 +0400

    Update menu.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     $('#menu-button').click(function (e) { 
-        // e.preventDefault();
         $('#menu').toggle();
-        $('#menu-button').toggle();
     });
 });
