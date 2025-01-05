d53a45c5477b8299e1ea3e4dd6dae28bfc5c8405
compuser21 <137155283+compuser21@users.noreply.github.com>
2025-01-05 16:02:32 +0400

    Add files via upload

---
+++ static/js/download-timer.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    let time = 60;
+    function formatTime(totalSeconds) {
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    setInterval(() => {
+        if (time != 0) {
+            time--;
+        }
+        $('#timer').text(formatTime(time)); 
+    }, 1000);
+
+    $('.download-button').click(function (e) { 
+        e.preventDefault();
+        $('#download-timer').show();
+    });
+});
